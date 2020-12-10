20ab0f36fb235000a9d2198cb1daff389b5d8dc9
ldevillard <legoexpliation@gmail.com>
2020-12-10 13:17:36 +0100

    commit

--- test.c
+++ test.c
@@ -6,11 +6,30 @@
 /*   By: ldevilla <ldevilla@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/04 17:25:31 by ldevilla          #+#    #+#             */
-/*   Updated: 2020/12/10 12:54:02 by ldevilla         ###   ########lyon.fr   */
+/*   Updated: 2020/12/10 13:17:00 by ldevilla         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "printf.h"
+#include <stdio.h>
+
+int		count_n(long n)
+{
+	int i;
+
+	i = 1;
+	if (n < 0)
+	{
+		n = -n;
+		i++;
+	}
+	while (n > 9)
+	{
+		n /= 10;
+		i++;
+	}
+	return (i);
+}
 
 int    analyse(char *str, va_list ap)
 {
@@ -24,7 +43,12 @@ int    analyse(char *str, va_list ap)
     if (str[i] == 'c')
 	{
         ft_putchar(va_arg(ap, int));
-		i ++;
+		i++;
+	}
+	else if (str[i] == 'd')
+	{
+		ft_putnbr(va_arg(ap, int));
+		i++; 
 	}
 	return (i);
 }
@@ -66,7 +90,9 @@ int	main(int ac, char **av)
 	(void)ac;
 	(void)av;
 	
-	ft_printf("Hey boy %c et toi %c aussi %c%c%     c\n", 'A', 'B', 'C', 'D', 'E');
-	printf("Hey boy %c et toi %c aussi %c%c%     c\n", 'A', 'B', 'C', 'D', 'E');
+	ft_printf("Hey boy %c tu as %d%d ans %d \n", 'A', 10000, 4, 2);
+	printf("Hey boy %c tu as %d%d ans %d \n", 'A', 10000, 4, 2);
+	
+	
 	return (0);
 }
