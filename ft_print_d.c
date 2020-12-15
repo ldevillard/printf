417a5951ecdcec539ee8858abeb8914d4155a446
ldevillard <legoexpliation@gmail.com>
2020-12-15 15:48:15 +0100

    commit

--- ft_print_d.c
+++ ft_print_d.c
@@ -6,11 +6,9 @@
 /*   By: ldevilla <ldevilla@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/14 12:09:07 by ldevilla          #+#    #+#             */
-/*   Updated: 2020/12/15 15:16:14 by ldevilla         ###   ########lyon.fr   */
+/*   Updated: 2020/12/15 15:46:28 by ldevilla         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "ft_printf.h"
 
 #include "ft_printf.h"
 
@@ -21,34 +19,42 @@ void	ft_print_d(t_struct *t_values, int nbr)
 	tmp = ft_itoa(nbr);
 	if (t_values->minus)
 	{
-		if (!(t_values->dot) && t_values->has_dot && nbr == 0)
-			ft_width(t_values->width, t_values->dot, 0, t_values);
-		else if (t_values->dot && (int)ft_strlen(tmp) < t_values->dot)
+		if (t_values->has_dot)
 		{
-			if (nbr < 0)
+			if (!(t_values->dot) && nbr == 0)
+				ft_width(t_values->width, t_values->dot, 0, t_values);
+			else if (nbr < 0)
 			{
-				ft_width(t_values->width, t_values->dot + 1, 0, t_values);
 				ft_putchar_print('-', t_values);
-				ft_width(t_values->dot, ft_strlen(tmp) - 1, 1, t_values);
+				if (t_values->dot)
+					ft_width(t_values->dot, ft_strlen(tmp) - 1, 1, t_values);
 				ft_putstr(tmp + 1);
+				if (t_values->dot)
+					ft_width(t_values->width, t_values->dot + 1, 0, t_values);
+				else
+					ft_width(t_values->width, ft_strlen(tmp), 0, t_values);
 				t_values->print += ft_strlen(tmp) - 1;
 			}
 			else
 			{
+
 				ft_width(t_values->dot, ft_strlen(tmp), 1, t_values);
 				ft_putstr(tmp);
-				ft_width(t_values->width, t_values->dot, 0, t_values);
+				if ((int)ft_strlen(tmp) < t_values->dot)
+					ft_width(t_values->width, t_values->dot, 0, t_values);
+				else
+					ft_width(t_values->width, ft_strlen(tmp), 0, t_values);
 				t_values->print += ft_strlen(tmp);
 			}
 		}
 		else
-		{		
+		{
 				ft_putstr(tmp);
 				ft_width(t_values->width, ft_strlen(tmp), 0, t_values);
 				t_values->print += ft_strlen(tmp);
 		}		
 	}
-	else if (!(t_values->minus))
+	else
 	{
 		if (!(t_values->dot) && t_values->has_dot && nbr == 0)
 			ft_width(t_values->width, t_values->dot, 0, t_values);
