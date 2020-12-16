8d611b1609e2f7049d7663151ee72fe1b35bdf47
ldevillard <logandvllrd@gmail.com>
2020-12-16 15:26:52 +0100

    norm

--- ft_print_p.c
+++ ft_print_p.c
@@ -6,7 +6,7 @@
 /*   By: ldevilla <ldevilla@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/16 12:56:09 by ldevilla          #+#    #+#             */
-/*   Updated: 2020/12/16 14:34:27 by ldevilla         ###   ########lyon.fr   */
+/*   Updated: 2020/12/16 15:16:41 by ldevilla         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -67,6 +67,24 @@ static	void	ft_low_hexa(char *tmp)
 	}
 }
 
+static void		ft_not_minus(t_struct *t_values, char *tmp, int nbr)
+{
+	if (!(t_values->dot) && t_values->has_dot && nbr == 0)
+		ft_width(t_values->width, t_values->dot, 0, t_values);
+	else if (t_values->dot)
+	{
+		if ((int)ft_strlen(tmp) < t_values->dot)
+			ft_width(t_values->width, t_values->dot, 0, t_values);
+		else
+			ft_width(t_values->width, ft_strlen(tmp), 0, t_values);
+		ft_width(t_values->dot, ft_strlen(tmp), 1, t_values);
+		ft_putstr(tmp);
+		t_values->print += ft_strlen(tmp);
+	}
+	else
+		ft_print_u_utils(t_values, tmp);
+}
+
 void			ft_print_p(t_struct *t_values, unsigned long nbr)
 {
 	char *tmp;
@@ -77,22 +95,7 @@ void			ft_print_p(t_struct *t_values, unsigned long nbr)
 	if (t_values->minus)
 		ft_minus(t_values, tmp, nbr);
 	else
-	{
-		if (!(t_values->dot) && t_values->has_dot && nbr == 0)
-			ft_width(t_values->width, t_values->dot, 0, t_values);
-		else if (t_values->dot)
-		{
-			if ((int)ft_strlen(tmp) < t_values->dot)
-				ft_width(t_values->width, t_values->dot, 0, t_values);
-			else
-				ft_width(t_values->width, ft_strlen(tmp), 0, t_values);
-			ft_width(t_values->dot, ft_strlen(tmp), 1, t_values);
-			ft_putstr(tmp);
-			t_values->print += ft_strlen(tmp);
-		}
-		else
-			ft_print_u_utils(t_values, tmp);
-	}
+		ft_not_minus(t_values, tmp, nbr);
 	t_values->i++;
 	free(tmp);
 }
--- ft_utils.c
+++ ft_utils.c
@@ -6,46 +6,11 @@
 /*   By: ldevilla <ldevilla@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/12/10 12:11:07 by ldevilla          #+#    #+#             */
-/*   Updated: 2020/12/16 14:38:45 by ldevilla         ###   ########lyon.fr   */
+/*   Updated: 2020/12/16 15:06:51 by ldevilla         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
-
-void	ft_print_struct(t_struct *t_values)
-{
-	ft_putchar('\n');
-	ft_putstr("PRINTED : ");
-	ft_putnbr(t_values->print);
-	ft_putchar('\n');
-	ft_putstr("PRECISION : ");
-	ft_putnbr(t_values->dot);
-	ft_putchar('\n');
-	ft_putstr("HAS DOT : ");
-	ft_putnbr(t_values->has_dot);
-	ft_putchar('\n');
-	ft_putstr("WIDTH : ");
-	ft_putnbr(t_values->width);
-	ft_putchar('\n');
-	ft_putstr("TYPE : ");
-	ft_putchar(t_values->type);
-	ft_putchar('\n');
-	ft_putstr("STAR : ");
-	ft_putnbr(t_values->star);
-	ft_putchar('\n');
-	ft_putstr("ZERO : ");
-	ft_putnbr(t_values->zero);
-	ft_putchar('\n');
-	ft_putstr("MINUS : ");
-	ft_putnbr(t_values->minus);
-	ft_putchar('\n');
-	ft_putstr("i : ");
-	ft_putnbr(t_values->i);
-	ft_putchar('\n');
-	ft_putstr("TMP : ");
-	ft_putstr(t_values->tmp);
-	ft_putchar('\n');
-}
 
 void	ft_putstr_l(char *str, int lengt)
 {
